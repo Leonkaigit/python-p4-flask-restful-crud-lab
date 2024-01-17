04fea59dccb1bf582634ffe78e8bb7429da2ab84
leongitonga <leon.kai.33@gmail.com>
2024-01-17 10:43:04 +0300

    lab done

--- server/app.py
+++ server/app.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
 from models import db, Plant
 
-app = Flask(__name__)
+app = Flask(__name__)  # Fix the typo here
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
@@ -15,7 +13,6 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
-
 
 class Plants(Resource):
 
@@ -37,19 +34,33 @@ class Plants(Resource):
 
         return make_response(new_plant.to_dict(), 201)
 
-
 api.add_resource(Plants, '/plants')
-
 
 class PlantByID(Resource):
 
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        
+        data = request.get_json()
+        
+        plant.is_in_stock = data.get('is_in_stock', plant.is_in_stock)
+        db.session.add(plant)     
+        db.session.commit()
+        return make_response(jsonify(plant.to_dict()), 200)
 
-
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        
+        db.session.delete(plant)
+        db.session.commit()
+        
+        return make_response("", 204)
+    
 api.add_resource(PlantByID, '/plants/<int:id>')
 
-
-if __name__ == '__main__':
+if __name__ == '__main__':  
     app.run(port=5555, debug=True)
